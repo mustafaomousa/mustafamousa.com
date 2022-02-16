6e707b9e61dd0f92e7aa234430e323340ea4eb41
mustafaomousa <mustafaomousa@icloud.com>
2022-02-16 09:35:41 -0600

    changed home padding

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import proud from "./proud-se.svg";
 const Home = () => {
   return (
     <section id="home">
-      <div className="flex items-center justify-center flex-wrap min-h-screen">
+      <div className="flex items-center justify-center flex-wrap min-h-[50vh]">
         <div className="space-y-2 flex flex-col ml-10">
           <p className="antialiased text-white text-2xl md:text-4xl lg:text-6xl ">
             Hey there
